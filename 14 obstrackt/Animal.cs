b7131df6447860b2604b07248a249622ae685931
Александр Иншаков <a.inshakov84@gmail.com>
2022-02-09 13:38:53 +0300

    Добавьте файлы проекта.

---
+++ 14 obstrackt/Animal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_obstrackt
+{
+   
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal (string name)
+        {
+            Name = name;
+        }
+        public abstract void GetTaste();
+
+        public  void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            GetTaste();
+        }
+    }
+
+    class Dog : Animal
+    {
+        string country;
+        string taste;
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name=value;
+        }
+
+        public Dog(string name, string taste, string country)
+            : base(name)
+        {
+            this.taste = taste;
+            this.country = country;
+        }
+
+        public Dog(string name) : base(name)
+        {
+        }
+
+        public override void GetTaste()
+        {
+            Console.WriteLine(taste);
+        }
+    }
+    class Cat : Dog
+    {
+        int level;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Cat(string name, int level)
+            : base(name)
+        {
+            this.level = level;
+        }
+        public override void GetTaste()
+        {
+            Console.WriteLine("Мау");
+        }
+    }
+}
---
+++ 14 obstrackt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_obstrackt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Овчарка", "черная", "Россия");
+            dog.ShowInfo();
+            Cat cat = new Cat("мурка", 1);
+            cat.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
